9cc8f2018a6efa99608879aacc98b6a8cdf4d76a
ombaejr <ombaejr@gmail.com>
2019-08-27 08:25:30 +0300

    Add the index page view function

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.shortcuts import render,redirect
+import datetime as dt
+from django.http  import HttpResponse,Http404
+from .models import Image,Location
 # Create your views here.
+
+def index(request):
+    location = Location.objects.all()
+    images=Image.objects.all()
+    return render(request,'home.html',{"images":images, "location": location})
