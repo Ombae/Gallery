02d38089a15944453b3831a441483e433723d818
ombaejr <ombaejr@gmail.com>
2019-08-27 11:04:41 +0300

    Correct tear down method for tests

--- photos/tests.py
+++ photos/tests.py
@@ -46,3 +46,8 @@ class ImageTestClass(TestCase):
 
         self.image = Image(id=1,image_name="TopBar",image_description="This is the top of the world",category=self.test_category,location=self.location,)
         self.image.save_image()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
