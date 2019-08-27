3cfc1946e1e50b5a5e0df5522c1eaef89faa97f6
ombaejr <ombaejr@gmail.com>
2019-08-27 10:49:30 +0300

    Create migration and write location test class

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.test import TestCase
+from .models import Location,Category,Image
+import datetime as dt
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.Nairobi= Location(location='Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Nairobi,Location))
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_save_method(self):
+        self.Nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
