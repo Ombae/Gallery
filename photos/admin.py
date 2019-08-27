dcb48771a8abec021b2389f8695dc68ca383862a
ombaejr <ombaejr@gmail.com>
2019-08-27 10:57:24 +0300

    Create admin superuser and add methods in admin.py

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
+from .models import Location,Category,Image
 
 # Register your models here.
+# class ImageAdmin(admin.ModelAdmin):
+#     filter_horizontal =('category',)
+
+
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
