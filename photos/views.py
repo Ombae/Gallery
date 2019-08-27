4e14ec579856d6fbab5dbe74b8257e781269f358
ombaejr <ombaejr@gmail.com>
2019-08-27 08:28:57 +0300

    Write functions for urls of search by category or location results

--- photos/views.py
+++ photos/views.py
@@ -21,3 +21,22 @@ def index2(request):
     location = Location.objects.all()
     images=Image.objects.all()
     return render(request,'home2.html',{"images":images, "location": location})
+
+def search_by_category(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        print(search_term)
+        found_images = Image.search_by_category(search_term)
+        print(found_images)
+        message = f"{search_term}"
+        return render(request,'search.html',{"message":message,"images":found_images})
+    else:
+        message="No searched category"
+        return render(request,'search.html',{"message":message})
+
+def search_by_location(request, location):
+    locations = Location.objects.all()
+    images = Image.search_by_location(location)
+    print(images)
+    title = f'{location} Photos'
+    return render(request, 'location.html', {'title': title, 'images': images, 'locations': locations})
